8715946f8f71dc5da6a3a073493ab5d4093ce31e
[dyad] <itzanu.cyber@gmail.com>
2025-09-11 18:29:45 +0530

    [dyad] Removed all pages except the home page - wrote 1 file(s), deleted 13 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import Issues from "./pages/Issues";
-import AchievementsPage from "./pages/AchievementsPage";
-import Settings from "./pages/Settings";
-import Records from "./pages/Records";
-import Leaderboard from "./pages/Leaderboard";
 
 const queryClient = new QueryClient();
 
@@ -21,12 +15,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/issues" element={<Issues />} />
-          <Route path="/achievements" element={<AchievementsPage />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/records" element={<Records />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
